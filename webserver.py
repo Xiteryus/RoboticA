28aa985aeac507269b06e905c2ab6681c42fdeef
Xiteryus <122489767+Xiteryus@users.noreply.github.com>
2025-06-14 21:06:15 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,13 +1,16 @@
 # -*- coding: Windows-1252 -*-
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, Response, send_file
 from temp import Adeept_SPI_LedPixel
 import atexit  # Pour exécuter une fonction à l'arrêt du serveur
 
 from led_controller import * 
 from motor import * 
 from led_techno import * 
+from servo_reboot import *
+from camera import *
 
-#init = 90
+camera = PiCameraStream()
+
 
 app = Flask(__name__)
 
@@ -47,14 +50,42 @@ def leds():
 def controle_moteur(): 
     direction = request.form.get("direction")
     if direction == "haut":
+        
         forward()
     elif direction == "bas":
         backward()
-    elif direction == "gauche": 
-        init = left(init)
-    elif direction == "droite":
-        init = right(init)
+    elif direction in ["gauche","droite"]: 
+        left_right(direction)
+    elif direction == "stop":
+        motorStop()
     return redirect(url_for('home'))
+    
+@app.route('/angle_reboot', methods=['POST'])
+def angle_reboot():
+  reboot()
+  return redirect(url_for('home'))
+  
+#camera 
+def gen():
+        while True:
+            frame = camera.get_frame()
+            if frame:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+  
+@app.route('/video_feed')
+def video():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/take_photo',methods=['GET'])
+def take_photo():
+  photo = camera.photo("Pictures/photo.png")
+  return send_file(photo, mimetype='image.png')
+  
 
 def webserver():
     app.run(host='192.168.12.1', port=8000)
+
+
+
+
